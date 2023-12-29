bac8b452b9c3a955c82a90022c45557dee3d9059
weslleylancador <138718819+weslleylancador@users.noreply.github.com>
2023-12-29 18:38:38 -0300

    H2 Database, JPA and more

--- src/main/java/org/springboot01/springboot01/entities/User.java
+++ src/main/java/org/springboot01/springboot01/entities/User.java
@@ -1,11 +1,17 @@
 package org.springboot01.springboot01.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final Long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;

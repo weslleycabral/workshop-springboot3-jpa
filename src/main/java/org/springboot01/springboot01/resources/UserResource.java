d342fc5d4f2e86b33a16025cd08fe5ea935902fb
Weslley Juan <weslley.deve@gmail.com>
2024-01-16 23:51:02 -0300

    User delete

--- src/main/java/org/springboot01/springboot01/resources/UserResource.java
+++ src/main/java/org/springboot01/springboot01/resources/UserResource.java
@@ -40,4 +40,10 @@ public class UserResource {
                         .toUri();
         return ResponseEntity.created(uri).body(user);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
